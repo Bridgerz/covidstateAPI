9355f9a68efe7285a8732f30cc55cc119b29cacb
Bridgerz <bridgerzoske@gmail.com>
2020-03-22 20:30:20 -0700

    allow CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import requests
 from flask_apscheduler import APScheduler
 from datetime import datetime
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
